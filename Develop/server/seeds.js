5765b9c5c1c9ad460549c44943b119b972b0041a
yassin alisheikh <yassinnawed@gmail.com>
2022-02-19 11:27:01 -0800

    started seeds and connected to db

--- Develop/server/seeds.js
+++ Develop/server/seeds.js
@@ -1,10 +1,33 @@
+const Country = require("./models/Country.model");
+const Recipe = require("./models/recipe.model");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//connection to the DB
+mongoose.connect(process.env.DATABASE_URI, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log("success")
+    }
+})
+
+// List of countries
 const countries = [
-    {name: "USA", continent: "America", population: 400e6, typeOfFood: "multiple"},
-    {name: "Pakistan", continent: "Asia", population: 250e6, typeOfFood: "multiple"},
-    {name: "Morocco", continent: "africa", population: 18e6, typeOfFood: "multiple"},
-    {name: "india", continent: "Asia", population: 1e9, typeOfFood: "plant"},
+    { name: "USA", countryCode: "us", continent: "America", population: 500e6, typesOfFood: "multiple" },
 ]
 
+// List of recipies
 const recipies = [
-    {}
+    { name: "Tea", userId: "123abc", countryCode: "us", ingridients: ["Tea", "Milk", "sugar"], process: "Mix the ingridients and put on fire", spiceLevel: 0, }
 ]
+
+//Insert countries and recipies to DB
+Country.insertMany(countries)
+.then(() => console.log("Added countries to DB"))
+.catch(err => console.log("Error while adding countries to DB", err));
+
+Recipe.insertMany(recipies)
+.then(() => console.log("Added recipies to DB"))
+.catch(err => console.log("Error while adding recipies to DB", err));
