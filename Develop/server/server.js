0a7075afbdcf85b4f50740138238ef5b6d9a016e
Paul McDevitt <paul.mcd1@icloud.com>
2022-02-25 20:07:58 -0500

    first heroku push

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -5,11 +5,14 @@ const { ApolloServer, gql } = require("apollo-server");
 const schema = fs.readFileSync("./schema.gql", "utf8");
 const resolvers = require("./resolvers/index.js");
 const typeDefs = gql(schema);
+const dotenv = require("dotenv");
+
+const PORT = process.env.PORT || 3000;
 // database url
 require("dotenv").config();
 
 // create a conncetion with the database
-mongoose.connect(process.env.DATABASE_URI, (error) => {
+mongoose.connect(process.env.MONGODB_URI , (error) => {
     if (error) {
         console.log(error)
     }
@@ -34,7 +37,7 @@ const server = new ApolloServer({
     })
 });
 
-server.listen().then(({ url }) => {
+server.listen(PORT).then(({ url }) => {
     console.log(`Server running: ${url}`)
 })
 
